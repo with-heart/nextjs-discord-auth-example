fc01fe70969ae41a11c6ca2597521e08a70c4db1
with-heart <with.heart+git@pm.me>
2022-04-23 19:48:10 -0400

    add catch all api route for next-auth

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,15 @@
+import NextAuth from 'next-auth'
+import DiscordProvider from 'next-auth/providers/discord'
+
+// https://discord.com/developers/docs/topics/oauth2#shared-resources-oauth2-scopes
+const scopes = ['identify']
+
+export default NextAuth({
+  providers: [
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET,
+      authorization: {params: {scope: scopes.join(' ')}},
+    }),
+  ],
+})
