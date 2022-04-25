d08df6cf77450f5e5b5aad09543bdde13ba22326
with-heart <with.heart+git@pm.me>
2022-04-24 21:59:49 -0400

    display user details when signed in

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,25 @@
 import type {NextPage} from 'next'
 import {signIn, signOut, useSession} from 'next-auth/react'
+import Image from 'next/image'
 
 const Home: NextPage = () => {
   const {data: session} = useSession()
 
   if (session) {
+    const {user} = session
+
     return (
       <>
-        You&apos;re signed in! Congratulations! <br />
+        {user?.image && (
+          <Image
+            src={user.image}
+            alt=""
+            width={38}
+            height={38}
+            style={{borderRadius: '50%'}}
+          />
+        )}
+        Hello, {user?.name}!<br />
         <button onClick={() => signOut()}>Sign out</button>
       </>
     )
